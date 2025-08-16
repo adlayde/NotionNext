4d6641edfe2821aab24b290ee17d85f48d0fc41e
adlayde <258192961@qq.com>
2025-08-16 20:19:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Estara', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '不待春风慢，我以明月宴群山', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '我曾经捡到过一束光，在日落前还给了太阳', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://estara.blog', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Estara,博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '几时归去，作个闲人, 对一张琴,一壶酒,一溪云',
+    '几时归去，作个闲人, 对一张琴,一壶酒,一溪云☁️',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
